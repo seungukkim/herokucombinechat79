7a7c041e297db8e7242141356dd7ce2acba209fa
seungukkim <gogobattle@naver.com>
2022-08-11 17:45:11 +0900

    update

--- app/main.py
+++ app/main.py
@@ -131,13 +131,14 @@ def whereLive():
     list2=list1[0]
     print(list2)
     print(type(list2))
+    list3=list2[2:-2]
     responseBody = {
         "version": "2.0",
         "template": {
             "outputs": [
                 {
                     "simpleText": {
-                        "text": list2
+                        "text": list3
                     }
                 }
             ]
